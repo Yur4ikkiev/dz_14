f9ebb74f79968a8695633cb97ac76df0fbdb3c9f
yura demyanenko <setka000@gmail.com>
2023-10-18 11:35:00 +0300

    dz14

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ let numberOne = +prompt("Введите любое число");
 let numberTwo = +prompt("Введите ещё любое число");
 let numberThree = +prompt("Введите ещё любое число");
 
-alert(`${numberOne} + ${numberTwo} + ${numberThree} / 3 = ${(numberOne + numberTwo + numberThree)/3}`);
+alert(`${numberOne} + ${numberTwo} + ${numberThree} / 3 = ${(numberOne + numberTwo + numberThree)/3}
+`);
 
 
 

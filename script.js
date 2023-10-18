69d931bb850f4214582702e745269a7307724088
yura demyanenko <setka000@gmail.com>
2023-10-18 11:19:11 +0300

    change dz14

--- script.js
+++ script.js
@@ -2,9 +2,7 @@ let numberOne = +prompt("Введите любое число");
 let numberTwo = +prompt("Введите ещё любое число");
 let numberThree = +prompt("Введите ещё любое число");
 
-alert(`${numberOne} + ${numberTwo} + ${numberThree} / 3 = 
-${(numberOne + numberTwo + numberThree)/3}
-`);
+alert(`${numberOne} + ${numberTwo} + ${numberThree} / 3 = ${(numberOne + numberTwo + numberThree)/3}`);
 
 
 
